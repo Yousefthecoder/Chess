f395574732105a4a7a180f7ca657d5a5d43ca3a0
MalekAlSabawi <168200593+MalekAlSabawi@users.noreply.github.com>
2024-05-01 09:38:47 -0400

    chess make game

--- chess.py
+++ chess.py
@@ -1,2 +1,49 @@
 #hey yoopy
 print("hey buddy")
+    
+# 1 pawn
+# 3 Knight
+# 4 Bishop
+# 5 Rook
+# 8 Queen
+# 9 King
+
+board = [ 
+            [5, 3, 4, 9, 8, 4, 3, 5], 
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [5, 3, 4, 9, 8, 4, 3, 5]
+                                        ]
+
+print(board[1][1])
+
+class chess_pieces:
+     Color = ""
+
+class King(chess_pieces): 
+    print("hey buddy")
+
+
+class Queen(chess_pieces): 
+     print("hey buddy")
+
+
+class Knight(chess_pieces): 
+     print("hey buddy")
+
+
+class Bishop(chess_pieces): 
+     print("hey buddy")
+
+     
+class Rook(chess_pieces): 
+     print("hey buddy")
+
+
+class Pawn(chess_pieces): 
+    print("hey buddy")
+
