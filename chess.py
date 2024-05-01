072ea41a6f209766f56afa6eb3708f2b26cd9db6
Yousefthecoder <168200555+Yousefthecoder@users.noreply.github.com>
2024-05-01 09:41:14 -0400

    chessssssssss

--- chess.py
+++ chess.py
@@ -1,3 +1,13 @@
+print("hey buddy")
+
+# 1 pawn
+# 3 Knight
+# 4 Bishop
+# 5 Rook
+# 8 Queen
+# 9 King
+
+
 row_1 = [0, 0, 0, 0, 0, 0, 0, 0]
 row_2 = [0, 0, 0, 0, 0, 0, 0, 0]
 row_3 = [0, 0, 0, 0, 0, 0, 0, 0]
@@ -5,6 +15,37 @@ row_4 = [0, 0, 0, 0, 0, 0, 0, 0]
 row_5 = [0, 0, 0, 0, 0, 0, 0, 0]
 row_6 = [0, 0, 0, 0, 0, 0, 0, 0]
 row_7 = [0, 0, 0, 0, 0, 0, 0, 0]
-row_8 = [0, 0, 0, 0, 0, 0, 0, 0]
+row_8 = [1, 0, 0, 0, 0, 0, 0, 0]
 
 board = [row_1, row_2, row_3, row_4, row_5, row_6, row_7, row_8]
+
+
+print(board[7][0])
+
+
+class chess_pieces:
+    Color = ""
+
+
+class King(chess_pieces):
+    print("hey buddy")
+
+
+class Queen(chess_pieces):
+    print("hey buddy")
+
+
+class Knight(chess_pieces):
+    print("hey buddy")
+
+
+class Bishop(chess_pieces):
+    print("hey buddy")
+
+
+class Rook(chess_pieces):
+    print("hey buddy")
+
+
+class Pawn(chess_pieces):
+    print("hey buddy")
