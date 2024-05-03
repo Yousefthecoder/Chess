93704fae16eb7b6cfafa82d502b09d4eddaf33a5
Yousefthecoder <168200555+Yousefthecoder@users.noreply.github.com>
2024-05-03 10:08:39 -0400

    Main branch code

--- chess.py
+++ chess.py
@@ -26,8 +26,6 @@ while run:
     LW = 100
     colour = [white, green]
 
-    # (x + y) % 2 if 0 then white esle green
-
     i = 0
     for x in range(0, 8 * LW, LW):
         for y in range(0, 8 * LW, LW):
